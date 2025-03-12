7b69ab58d53096542b28740a746d39f97c8c3df0
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 08:15:38 +0700

    menambahkan kelas produk

--- KodeProduk.cs
+++ KodeProduk.cs
@@ -8,5 +8,24 @@ namespace modul4_103022300134
 {
     class KodeProduk
     {
+        public string GetKodeProduk(string elektronik)
+        {
+            Dictionary<string, string> kodeProduk = new Dictionary<string,string>
+                
+            {
+                { "Laptop ", "E100" },
+                { "Smartphone", "E101" },
+                { "Tablet", "E102" },
+                { "Headset", "E103" },
+                { "Keyboard", "E104" },
+                { "Mouse", "E105" },
+                { "Printer", "E106" },
+                { "Monitor", "E107" },
+                { "Smartwatch", "E108" },
+                { "Kamera", "E109" }
+            };
+            return kodeProduk.ContainsKey(elektronik) ? kodeProduk[elektronik] : "KodeProduk tidak ditemukan"; 
+        }
     }
 }
+    
